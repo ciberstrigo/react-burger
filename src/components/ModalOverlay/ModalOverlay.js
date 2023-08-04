2d111e508349d45cb45bd52f6c24d533f28df6ba
Leonid Averyanov <ciberstrigo@gmail.com>
2023-08-05 01:28:58 +0400

    step-2: Обработчик Escape помещён внутрь useEffect

--- src/components/ModalOverlay/ModalOverlay.js
+++ src/components/ModalOverlay/ModalOverlay.js
@@ -1,42 +1,40 @@
-import ReactDOM from 'react-dom';
-import styles from './ModalOverlay.module.css';
+import ReactDOM from "react-dom";
+import styles from "./ModalOverlay.module.css";
 import PropTypes from "prop-types";
 import React from "react";
 
 const modalRoot = document.getElementById("modals");
 
 const ModalOverlay = ({ children, onClick }) => {
+  React.useEffect(() => {
     const handleEsc = (e) => {
-        if (e.key === "Escape") {
-            onClick();
-        }
-    }
+      if (e.key === "Escape") {
+        onClick();
+      }
+    };
 
-    React.useEffect(() => {
-        document.addEventListener("keydown", handleEsc, false);
+    document.addEventListener("keydown", handleEsc, false);
 
-        return () => {
-            document.removeEventListener("keydown", handleEsc, false);
-        };
-    }, []);
+    return () => {
+      document.removeEventListener("keydown", handleEsc, false);
+    };
+  }, []);
 
-    if(modalRoot) {
-        return ReactDOM.createPortal(
-            (
-                <div className={styles.root} onClick={onClick}>
-                    {children}
-                </div>
-            ),
-            modalRoot
-        );
-    } else {
-        return null;
-    }
+  if (modalRoot) {
+    return ReactDOM.createPortal(
+      <div className={styles.root} onClick={onClick}>
+        {children}
+      </div>,
+      modalRoot,
+    );
+  } else {
+    return null;
+  }
 };
 
 ModalOverlay.propTypes = {
-    children: PropTypes.element,
-    onClick: PropTypes.func
+  children: PropTypes.element,
+  onClick: PropTypes.func,
 };
 
 export default ModalOverlay;

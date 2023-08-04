26ec6f3b85d1b21b5d9d0219eeef0c65083e6d0e
Leonid Averyanov <ciberstrigo@gmail.com>
2023-08-05 01:12:24 +0400

    step-2: поправлены отступы

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,68 +1,81 @@
 import React from "react";
 import AppHeader from "../AppHeaders/AppHeader";
 import BurgerIngredients from "../BurgerIngridients/BurgerIngridients";
-import BurgerConstructor from "../BurgerConstructor/BurgerConstructor"
-import style from "./App.module.css"
+import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
+import style from "./App.module.css";
 import Modal from "../Modal/Modal";
 import IngredientDetails from "../IngredientDetails/IngredientDetails";
 import OrderDetails from "../OrderDetails/OrderDetails";
 
-const API_URL = 'https://norma.nomoreparties.space/api';
+const API_URL = "https://norma.nomoreparties.space/api";
 
 const App = () => {
-    const [orderVisible, setOrderVisible] = React.useState(false);
-    const [currentIngredient, setCurrentIngredient] = React.useState(null);
+  const [orderVisible, setOrderVisible] = React.useState(false);
+  const [currentIngredient, setCurrentIngredient] = React.useState(null);
 
-    const [assortment, setAssortment] = React.useState();
-    const [burgerBun, setBurgerBun] = React.useState();
-    const [ingredients, setIngredients] = React.useState();
+  const [assortment, setAssortment] = React.useState();
+  const [burgerBun, setBurgerBun] = React.useState();
+  const [ingredients, setIngredients] = React.useState();
 
-    React.useEffect(() => {
-        fetch(`${API_URL}/ingredients`)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                return Promise.reject(`Ошибка ${response.status}`);
-            })
-            .then(({data}) => {
-                setAssortment(data);
-                setBurgerBun(data.filter((e) => e.type === 'bun')[0]);
-                setIngredients(data.filter((e) => e.type !== 'bun'));
-            }).catch(error => {
-                console.error('Ошибка при получении данных:', error);
-            });
-    }, []);
+  React.useEffect(() => {
+    fetch(`${API_URL}/ingredients`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`Ошибка ${response.status}`);
+      })
+      .then(({ data }) => {
+        setAssortment(data);
+        setBurgerBun(data.filter((e) => e.type === "bun")[0]);
+        setIngredients(data.filter((e) => e.type !== "bun"));
+      })
+      .catch((error) => {
+        console.error("Ошибка при получении данных:", error);
+      });
+  }, []);
 
-    const toggleOrderDetails = () => {
-        setOrderVisible(!orderVisible)
-    }
+  const toggleOrderDetails = () => {
+    setOrderVisible(!orderVisible);
+  };
 
-    const showIngredientDetails = (ingredient) => {
-        setCurrentIngredient(ingredient ?? null);
-    }
+  const showIngredientDetails = (ingredient) => {
+    setCurrentIngredient(ingredient ?? null);
+  };
 
-    return (<main className={style.App__main}>
-        <AppHeader/>
-        <div className={style.App__content}>
-            <div className={style.App__container}>
-                <BurgerIngredients assortment={assortment}
-                                   showDetails={showIngredientDetails}
-                />
-                <BurgerConstructor ingredients={ingredients}
-                                   burgerBun={burgerBun}
-                                   showOrderDetails={toggleOrderDetails}
-                />
-            </div>
+  return (
+    <main className={style.App__main}>
+      <AppHeader />
+      <div className={style.App__content}>
+        <div className={style.App__container}>
+          <BurgerIngredients
+            assortment={assortment}
+            showDetails={showIngredientDetails}
+          />
+          <BurgerConstructor
+            ingredients={ingredients}
+            burgerBun={burgerBun}
+            showOrderDetails={toggleOrderDetails}
+          />
         </div>
-        {orderVisible && (<Modal onClick={toggleOrderDetails}>
-            <OrderDetails />
-        </Modal>)}
-        {currentIngredient &&
-            (<Modal header={"Детали ингредиента"} onClick={() => {showIngredientDetails(null)}}>
-                <IngredientDetails ingredient={currentIngredient}/>
-            </Modal>)}
-    </main>);
-}
+      </div>
+      {orderVisible && (
+        <Modal onClick={toggleOrderDetails}>
+          <OrderDetails />
+        </Modal>
+      )}
+      {currentIngredient && (
+        <Modal
+          header={"Детали ингредиента"}
+          onClick={() => {
+            showIngredientDetails(null);
+          }}
+        >
+          <IngredientDetails ingredient={currentIngredient} />
+        </Modal>
+      )}
+    </main>
+  );
+};
 
 export default App;

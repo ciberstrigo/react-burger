cbac88723df59349a27207dbc1245ddc6d17d2a2
Leonid Averyanov <ciberstrigo@gmail.com>
2023-08-05 01:18:50 +0400

    step-2: исправлено удаление слушателя события

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,83 +1,87 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import style from "./BurgerConstructor.module.css"
-import {ConstructorElement, CurrencyIcon, Button} from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
+import style from "./BurgerConstructor.module.css";
+import {
+  ConstructorElement,
+  CurrencyIcon,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import "@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css";
 import types from "../../utils/types";
 import BurgerConstructorItem from "../BurgerConstructorItem/BurgerConstructorItem";
 
-const BurgerIngredients = ({burgerBun, ingredients, showOrderDetails}) => {
-    const scrollableRef = React.useRef();
+const BurgerIngredients = ({ burgerBun, ingredients, showOrderDetails }) => {
+  const scrollableRef = React.useRef();
 
-    const adjustContainerHeight = () => {
-        const height = window.innerHeight - (window.innerHeight % 90);
-        scrollableRef.current.style.maxHeight = `calc(${height}px - 640px)`;
-    }
+  const adjustContainerHeight = () => {
+    const height = window.innerHeight - (window.innerHeight % 90);
+    scrollableRef.current.style.maxHeight = `calc(${height}px - 640px)`;
+  };
 
-    React.useEffect(() => {
-        adjustContainerHeight();
-        window.addEventListener('resize', adjustContainerHeight);
+  React.useEffect(() => {
+    adjustContainerHeight();
+    window.addEventListener("resize", adjustContainerHeight);
 
-        return () => {
-            document.removeEventListener('resize', adjustContainerHeight);
-        };
-    });
+    return () => {
+      window.removeEventListener("resize", adjustContainerHeight);
+    };
+  });
 
-    return (<section className={style.burgerConstructor}>
-        <div className={style.burgerConstructor__list}>
-            {
-                !!burgerBun && (
-                    <ConstructorElement
-                        type="top"
-                        text={`${burgerBun.name} (верх)`}
-                        price={burgerBun.price}
-                        thumbnail={burgerBun.image}
-                        extraClass={'mr-4'}
-                        isLocked={true}
-                    />
-                )
-            }
-            <div className={`${style.scrollable}`} ref={scrollableRef}>
-            {
-                ingredients &&
-                ingredients.map((e) =>
-                    e.type !== 'bun' && (
-                        <BurgerConstructorItem item={e} key={e._id}/>
-                    )
-                )
-            }
-            </div>
-            {
-                !!burgerBun && (
-                    <ConstructorElement
-                        type="bottom"
-                        text={`${burgerBun.name} (низ)`}
-                        price={burgerBun.price}
-                        thumbnail={burgerBun.image}
-                        extraClass={'mr-4'}
-                        isLocked={true}
-                        />
-                )
-            }
-            <div className={style.makeOrderBlock}>
-                <div className={style.priceBlock}>
-                <span className={style.priceBlock__number}>
-                    610
-                </span>
-                    <CurrencyIcon type="primary"/>
-                </div>
-                <Button htmlType="button" type="primary" size="large" onClick={showOrderDetails}>
-                    Оформить заказ
-                </Button>
-            </div>
+  return (
+    <section className={style.burgerConstructor}>
+      <div className={style.burgerConstructor__list}>
+        {!!burgerBun && (
+          <ConstructorElement
+            type="top"
+            text={`${burgerBun.name} (верх)`}
+            price={burgerBun.price}
+            thumbnail={burgerBun.image}
+            extraClass={"mr-4"}
+            isLocked={true}
+          />
+        )}
+        <div className={`${style.scrollable}`} ref={scrollableRef}>
+          {ingredients &&
+            ingredients.map(
+              (e) =>
+                e.type !== "bun" && (
+                  <BurgerConstructorItem item={e} key={e._id} />
+                ),
+            )}
         </div>
-    </section>);
-}
+        {!!burgerBun && (
+          <ConstructorElement
+            type="bottom"
+            text={`${burgerBun.name} (низ)`}
+            price={burgerBun.price}
+            thumbnail={burgerBun.image}
+            extraClass={"mr-4"}
+            isLocked={true}
+          />
+        )}
+        <div className={style.makeOrderBlock}>
+          <div className={style.priceBlock}>
+            <span className={style.priceBlock__number}>610</span>
+            <CurrencyIcon type="primary" />
+          </div>
+          <Button
+            htmlType="button"
+            type="primary"
+            size="large"
+            onClick={showOrderDetails}
+          >
+            Оформить заказ
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 BurgerIngredients.propTypes = {
-    ingredients: PropTypes.arrayOf(types.ingredient),
-    burgerBun: types.ingredient,
-    showOrderDetails: PropTypes.func
+  ingredients: PropTypes.arrayOf(types.ingredient),
+  burgerBun: types.ingredient,
+  showOrderDetails: PropTypes.func,
 };
 
 export default BurgerIngredients;

a5f0ffeb9a0452837619c3a2c3f3d186aad1c932
Leonid Averyanov <ciberstrigo@gmail.com>
2023-08-09 20:38:06 +0400

    step-1: типы isRequired

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,31 +1,31 @@
 import PropTypes from "prop-types";
 
 const ingredient = PropTypes.shape({
-    _id: PropTypes.string,
-    name: PropTypes.string,
-    price: PropTypes.number,
-    image: PropTypes.string,
-    image_large: PropTypes.string,
-    image_mobile: PropTypes.string,
-    calories: PropTypes.number,
-    carbohydrates: PropTypes.number,
-    fat: PropTypes.number,
-    proteins: PropTypes.number,
-    type: PropTypes.string
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    proteins: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired
 })
 
 const types = {
     ingredient: ingredient,
     order: PropTypes.shape({
-        name: PropTypes.string,
+        name: PropTypes.string.isRequired,
         order: PropTypes.shape({
-            number: PropTypes.number
-        }),
-        success: PropTypes.bool
+            number: PropTypes.number.isRequired
+        }).isRequired,
+        success: PropTypes.bool.isRequired
     }),
     burger: PropTypes.shape({
-        bun: ingredient,
-        ingredients: PropTypes.arrayOf(ingredient)
+        bun: ingredient.isRequired,
+        ingredients: PropTypes.arrayOf(ingredient).isRequired
     })
 };
 

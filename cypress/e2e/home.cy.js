483afb84d3e2419a8418d1d8d1e001203ead6f37
Leonid Averianov <ciberstrigo@gmail.com>
2023-10-19 14:44:13 +0400

    Моки, конфиги, фиксы скриптов тестов

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,45 +1,49 @@
 import 'cypress-drag-drop';
 
+const dropToConstructor = (ingredientId) => {
+    const dataTransfer = new DataTransfer();
+    cy.get(ingredientId).trigger('dragstart', {dataTransfer});
+    cy.get('#drop').trigger('drop', {dataTransfer});
+};
+
 describe('testing App', () => {
     beforeEach(function () {
         cy.visit('http://localhost:3000');
-        // cy.viewport(1920, 1080);
+        cy.intercept("GET", "https://norma.nomoreparties.space/api/ingredients", {
+            fixture: "mockIngredients.json",
+        });
+        cy.intercept("POST", "https://norma.nomoreparties.space/api/orders", {
+            fixture: "mockMakeOrderResponse.json"
+        });
+        cy.intercept("POST", "https://norma.nomoreparties.space/api/auth/login", {
+            fixture: "loginFixture.json"
+        });
     })
 
     it('ingredient details modal', () => {
-        cy.visit('http://localhost:3000');
-
         cy.get('#643d69a5c3f7b9001cfa093c').click();
-        cy.wait(2000);
         cy.get('#modals').should('exist');
         cy.get('#modal_close_cross').click();
     });
 
-    it('moves random ingredients to constructor', () => {
-        cy.visit('http://localhost:3000');
-        cy.get('#643d69a5c3f7b9001cfa093c').drag('#drop');
-        cy.get('#643d69a5c3f7b9001cfa0942').drag('#drop');
-        cy.get('#643d69a5c3f7b9001cfa0941').drag('#drop');
-        cy.get('#643d69a5c3f7b9001cfa0946').drag('#drop');
-        cy.get('#643d69a5c3f7b9001cfa093c').drag('#drop');
-        // cy.get('#643d69a5c3f7b9001cfa0949').drag('#drop'); // Бага вот тут
+    it('moves random ingredients to constructor, making order and close modal', () => {
+        dropToConstructor('#643d69a5c3f7b9001cfa093c');
+        dropToConstructor('#643d69a5c3f7b9001cfa0942');
+        dropToConstructor('#643d69a5c3f7b9001cfa0941');
+        dropToConstructor('#643d69a5c3f7b9001cfa0946');
+        dropToConstructor('#643d69a5c3f7b9001cfa093c');
+        dropToConstructor('#643d69a5c3f7b9001cfa0949');
 
-        const dataTransfer = new DataTransfer();
-
-        cy.get('#643d69a5c3f7b9001cfa0949').trigger('dragstart', {dataTransfer});
-        cy.get('#drop').trigger('drop', {
-            dataTransfer
-        });
-
-        cy.wait(2000)
         cy.get('button').contains('Оформить заказ').click()
         cy.get('form')
-        cy.get('input[name=email]').type('ciberstrigo@gmail.com').should('have.value', 'ciberstrigo@gmail.com')
-        cy.get('input[name=password]').type('mangoworms').should('have.value', 'mangoworms')
+        cy.get('input[name=email]').type('test@data.com')
+        cy.get('input[name=email]').should('have.value', 'test@data.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('input[name=password]').should('have.value', 'password')
         cy.get('button').contains('Войти').click()
-        cy.wait(2000)
         cy.get('button').contains('Оформить заказ').click()
-        cy.wait(20000)
+        cy.get('#modals').contains('идентификатор заказа')
+        cy.get('#modals').contains('99999')
         cy.get('#modal_close_cross').click();
     });
 });
